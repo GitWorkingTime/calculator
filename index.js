17b4569016a32aeb76056065d6e960e97b125861
GitWorkingTime <dlee28262@gmail.com>
2025-12-25 23:18:47 -0500

    Added HTML base

--- index.js
+++ index.js
@@ -5,7 +5,6 @@
  * - multiply
  * - divide
  */
-
 function add(a, b) {
     return a + b;
 }
@@ -21,3 +20,32 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate( operator, a, b) {
+    switch(operator) {
+        case '+':
+            add(a, b);
+            break;
+        case '-':
+            subtract(a, b);
+            break;
+        case '*':
+            multiply(a, b);
+            break;
+        case '/':
+            divide(a, b);
+            break;
+    }
+}
+
+// ---- DOM Manipulation ----
+const calc = document.querySelector(".calc-body");
+const num = document.querySelector(".numbers");
+const op = document.querySelector(".operators");
+for( let i = 0; i < 10; ++i ) {
+    const numChild = document.createElement("button");
+    numChild.textContent = i;
+    numChild.setAttribute("class", `${i}`);
+    num.appendChild(numChild);
+}
+
