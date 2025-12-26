97fec409f3a2d75918ae65c877b402574c973ef8
GitWorkingTime <dlee28262@gmail.com>
2025-12-25 23:08:28 -0500

    Added core funcationality

---
+++ index.js
@@ -0,0 +1,23 @@
+/*
+ * Main functionality:
+ * - add
+ * - subtract
+ * - multiply
+ * - divide
+ */
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
